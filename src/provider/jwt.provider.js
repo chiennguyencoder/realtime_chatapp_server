998adccf57be941834458c11face1c9b8cfe1c02
Nguyen Minh Chien <nmchien116@gmail.com>
2025-07-08 21:13:14 +0700

    feat: update generate and verify token

---
+++ src/provider/jwt.provider.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
+
+const generateAccessToken = async (userData) => {
+    return new Promise((resolve, reject) => {
+        const payload = { userID }
+        const secret = process.env.ACCESS_TOKEN_SECRET || ""
+        const options =  {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRES_IN || "10m"
+        }
+
+        jwt.sign(payload, secret, options, (err, token) => {
+            if (err) reject(err)
+            else if (token){
+                resolve(token)
+            }
+            else reject(new Error('Tạo accessToken thất bại'))
+        })
+    })
+}
+
+export default generateAccessToken
